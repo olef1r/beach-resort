9548c1fc58fc56b6598db303a477f4dc88d2a422
Anastasiia Olefir <aolef17@gmail.com>
2019-10-29 15:26:31 +0200

    add context

--- src/components/FeaturedRooms.js
+++ src/components/FeaturedRooms.js
@@ -4,9 +4,9 @@ import { RoomContext } from '../context'
 export default class FeaturedRooms extends React.Component {
   static contextType = RoomContext;
   render() {
-    const { name, greeting } = this.context;
+
     return (
-      <div>hello {name} from {greeting}</div>
+      <div>hello from</div>
     )
   }
 }
--- src/context.js
+++ src/context.js
@@ -1,12 +1,30 @@
 import React from 'react';
-
+import items from './data';
+import { importDefaultSpecifier } from '@babel/types';
 const RoomContext = React.createContext();
 
 class RoomProvider extends React.Component {
   state = {
-    greeting: 'hello',
-    name: 'john'
+    rooms: [],
+    sortedRooms: [],
+    featuredRooms: [],
+    loading: true
   };
+
+  componentDidMount() {
+    let rooms = this.formatData(items);
+    console.log(rooms)
+  }
+
+  formatData(items) {
+    let tempItems  = items.map(item => {
+      let id = item.sys.id;
+      let images = item.fields.images.map(image => image.fields.file.url)
+      let room = {...item.fields, images, id};
+      return room;
+    });
+    return tempItems;
+  }
   render() {
     return (
       <RoomContext.Provider value={{...this.state}}>
